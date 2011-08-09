a8e71a336e5430d0dfec21e3ebfcb9e2fda982d2
Ed Silva <ed@litmusgreen.com>
2011-08-08 17:51:13 -0700

    sync up to svn
    Add changes made to last svn update.

--- SSCrypto.m
+++ SSCrypto.m
@@ -138,6 +138,10 @@
     while ((inlen = BIO_read(mem, inbuf, sizeof(inbuf))) > 0) {
         [data appendBytes: (const void *)inbuf length: inlen];
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> sync up to svn
     // Clean up and go home
     BIO_free_all(mem);
     return [NSData dataWithData:data];
